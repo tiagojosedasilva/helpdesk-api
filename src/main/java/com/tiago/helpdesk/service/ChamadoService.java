109ab55cc6d5d8bda5155eaf17617af8d04bb556
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2023-07-08 19:28:52 -0300

    Endpoint update para chamados

--- src/main/java/com/tiago/helpdesk/service/ChamadoService.java
+++ src/main/java/com/tiago/helpdesk/service/ChamadoService.java
@@ -11,6 +11,7 @@ import com.tiago.helpdesk.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,6 +38,13 @@ public class ChamadoService {
         return repository.save(newChamado(objDTO));
     }
 
+    public Chamado update(Integer id, ChamadoDTO objDTO){
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
+
     public Chamado newChamado(ChamadoDTO obj){
         Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
         Cliente cliente = clienteService.findById(obj.getCliente());
@@ -46,6 +54,11 @@ public class ChamadoService {
         if (obj.getId() != null){
             chamado.setId(obj.getId());
         }
+
+        if (obj.getStatus().equals(2)){
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
         chamado.setCliente(cliente);
         chamado.setTecnico(tecnico);
         chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
